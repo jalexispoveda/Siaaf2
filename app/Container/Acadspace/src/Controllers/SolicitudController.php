59f5a126ead8e1ef9d2026ccee42453abda60438
Edwin <edwinclavijo22@gmail.com>
2017-07-06 11:20:57 -0500

    Ruta, controlador y vista registro solicitud

---
+++ app/Container/Acadspace/src/Controllers/SolicitudController.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yeison Gomez
+ * Date: 19/06/2017
+ * Time: 2:20 PM
+ */
+
+namespace App\Container\Acadspace\src\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Container\Users\Src\Interfaces\UserInterface;
+use App\Container\Humtalent\src\Persona;
+use Illuminate\Support\Facades\DB;
+
+class SolicitudController extends Controller
+{
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "Estoy en Index";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('acadspace.registroSolicitud');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        //return "en el show";
+       // return view('humtalent.empleado.consultaEmpleado');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+    }
+
+}
